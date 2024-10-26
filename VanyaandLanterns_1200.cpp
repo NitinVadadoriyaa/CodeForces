78fb5e8c0b3c46e0d428d6337c9e7680f419063a
NitinVadadoriya <vadadoriyanitin55@gmail.com>
2024-10-26 10:25:00 +0530

    2024-10-26 challenge done!

--- VanyaandLanterns_1200.cpp
+++ VanyaandLanterns_1200.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using ll = long long;
 int mod = 1e9 + 7;
-
+typedef long double ld;
 
 //ctrl + space to run exsiting file.
 
@@ -17,24 +17,28 @@ int main() {
         int n,l;
         cin >> n >> l;
         vector<int> a(n);
-        double maxDis = 0.0;
+        long double maxDis = 0.0;
         for (int i = 0; i < n; i++)
         {
             cin >> a[i];   
         }
         sort(a.begin(), a.end());
-        int lastLoc = 0;
+        int lastLoc = -1;
         for (int i = 0; i < n; i++) {
-            if (lastLoc == 0 && a[i] != 0) {
-                maxDis = max(maxDis, a[i] + 0.0);
-            } else if (a[i] == l) {
-                maxDis = max(maxDis, a[i] - lastLoc + 0.0);
+            if (lastLoc == -1 && a[i] != 0) {
+                maxDis = max(maxDis, a[i] + (ld)0.0);
+            } else if (a[i] == l && lastLoc == -1) {
+                maxDis = max(maxDis, a[i] + (ld)0.0);
             } else {
-                maxDis = max(maxDis, ((a[i] - lastLoc) / 2.0));
+                maxDis = max(maxDis, ((a[i] - lastLoc) / (ld)2.0));
             }
             lastLoc = a[i];
         }
-       
-        cout << maxDis << endl;
+        if (a.back() != l) {
+            maxDis = max(maxDis, (ld)(l - a.back()+0.0));
+        }
+        // ld p = 22258199.5000000;
+
+        cout << fixed << setprecision(7) << maxDis<< endl;
         return 0;
 }
