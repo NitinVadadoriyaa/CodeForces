8f05fcef79a5d4202fdf226b157bdc674b683a6d
NitinVadadoriya <vadadoriyanitin55@gmail.com>
2024-10-30 09:09:53 +0530

    2024-10-30 challenge done!

---
+++ CoinRows_1300.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int mod = 1e9 + 7;
+
+
+//ctrl + space to run exsiting file.
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #else
+    #endif
+    int t;
+    cin >> t;
+    while (t) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+            t--;
+    }
+    return 0;
+}
