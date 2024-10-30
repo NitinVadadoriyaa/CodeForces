fd3ea4fee51918f891397e5c1dbdacbb248097ad
NitinVadadoriya <vadadoriyanitin55@gmail.com>
2024-10-30 11:22:10 +0530

    2024-10-30 challenge done!

--- CoinRows_1300.cpp
+++ CoinRows_1300.cpp
@@ -16,13 +16,45 @@ int main() {
     int t;
     cin >> t;
     while (t) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
+        int m;
+        cin >> m;
+        vector<int> firstRow(m),secondRow(m);
+        vector<int> prefixFirst(m, 0),prefixSecond(m,0);
+
+        for (int i = 0; i < m; i++)
+        {
+            cin >> firstRow[i];
         }
-            t--;
+        for (int i = 0; i < m; i++) 
+        {
+            cin >> secondRow[i];
+        }
+        for (int i = 0; i < m; i++) { // prefixFirst for second row
+            prefixFirst[i] = (i != 0) ? (prefixFirst[i - 1] + secondRow[i]) : (secondRow[i]);
+        }
+        for (int i = m - 1; i >= 0; i--) // prefixSecond for first row
+        {
+            prefixSecond[i] = (i != m - 1) ? (prefixSecond[i + 1] + firstRow[i]) : (firstRow[i]);
+        }
+
+        int ans = INT_MAX;
+        for (int i = 0; i < m; i++) 
+        {
+            int leftDow = (i - 1 >= 0) ? prefixFirst[i - 1] : INT_MAX;
+            int rightUp = (i + 1 < m) ? prefixSecond[i + 1] : INT_MAX;
+            if (leftDow == INT_MAX) {
+                leftDow = 0;
+            }
+            if (rightUp == INT_MAX) {
+                rightUp = (leftDow == 0) ? INT_MAX : 0;
+            }
+            ans = min(ans, max(leftDow, rightUp));
+            // cout << ans << endl;
+        }
+
+        cout << ((ans == INT_MAX) ? 0 : ans) << endl;
+//helo
+        t--;
     }
     return 0;
 }
